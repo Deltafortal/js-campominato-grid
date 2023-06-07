4d361e49c03409cd7442eb3d5793f7e531b937df
Deltafortal <aldolucchetta21@gmail.com>
2023-06-07 17:51:04 +0200

    Added bomb with javascript

--- js/script.js
+++ js/script.js
@@ -14,7 +14,9 @@ const grid = document.getElementById('grid');
 const row = 10;
 const column = 10;
 const totalCells = column * row;
+const bombChance = 20;
 
+let bombed = false;
 
 
 
@@ -31,19 +33,42 @@ const createCell = () => {
 }
 
 
+const bomb = () => {
+
+    const random = Math.floor(Math.random() * 100) + 1;
+
+    if(random <= bombChance) {
+        bombed = true;
+    }
+
+    return bombed
+}
+
+
 
 // Al click del bottone genero la griglia
 
 button.addEventListener('click', function(){
+    
 
     for (let i = 0; i < totalCells; i++) {
 
         const cell = createCell();
         cell.innerText = i + 1;
 
+        bomb();
+        if (bombed === true) {
+            cell.classList.add('bombed')
+            bombed = false;
+        }
+
         cell.addEventListener('click', function(){
             cell.classList.add('clicked');
             console.log(cell.innerText)
+
+            if (cell.classList.contains('bombed')) {
+                location.reload();
+            }
         }) 
 
         grid.appendChild(cell);
